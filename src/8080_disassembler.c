7598e296f2797054e0155007d72d65f641e448c4
romcol <e.rcolombo2@gmail.com>
2025-12-02 22:10:09 -0300

    feat: implement LXI opcodes

--- src/8080_disassembler.c
+++ src/8080_disassembler.c
@@ -27,3 +27,50 @@ int main(int argc, char** argv) {
     free(rom);
     return 0;
 }
+
+
+int disassemble_next_8080(uint8_t* rom, int pc) {
+    uint8_t* op = &rom[pc];
+    int opbytes = 1;
+
+    printf("%04X\t");
+    switch(*op) {
+        case 0x00: // NOP
+            printf("NOP\n");
+            break;
+
+        /*
+            LXI rp, data16 (Load register pair immediate)
+
+            (rh) <- (byte 3),
+            (rl) <- (byte 2)
+            Byte 3 of the instruction is moved into the high-order
+            register (rh) of the register pair rp. Byte 2 of the instruction is moved into the low-order register (rl) of
+            the register pair rp.
+
+            0|0|R|P|0|0|0|1
+        */
+        case 0x01: // LXI B - Pair B & C
+            printf("LXI B, %02x%02x\n", op[2], op[1]);
+            opbytes = 3;
+            break;
+        case 0x11: // LXI D - Pair D & E
+            printf("LXI D, %02x%02x\n", op[2], op[1]);
+            opbytes = 3;
+            break;
+        case 0x21: // LXI H - Pair H & L
+            printf("LXI H, %02x%02x\n", op[2], op[1]);
+            opbytes = 3;
+            break;
+        case 0x31: // LXI SP
+            printf("LXI SP, %02x%02x\n", op[2], op[1]);
+            opbytes = 3;
+            break;
+
+        default:
+            printf("UNKNOWN\n");
+            break;
+    }
+
+    return opbytes;
+}
