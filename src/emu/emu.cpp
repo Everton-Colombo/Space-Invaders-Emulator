2c80d222ce2391d6ffacdca0a9eea07880f08eaf
romcol <e.rcolombo2@gmail.com>
2025-12-06 17:27:43 -0300

    feat: implement LXI

--- src/emu/emu.cpp
+++ src/emu/emu.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 #include <cstdio>
 
-uint8_t* CPU_8080::_getByteAddr(SrcDestId8080 id) {
+uint8_t* CPU_8080::_getAddr(SrcDestId8080 id) {
     switch (id) {
         case SrcDestId8080::A: return &this->registers.a;
         case SrcDestId8080::B: return &this->registers.b;
@@ -12,16 +12,21 @@ uint8_t* CPU_8080::_getByteAddr(SrcDestId8080 id) {
         case SrcDestId8080::H: return &this->registers.h;
         case SrcDestId8080::L: return &this->registers.l;
 
-        case SrcDestId8080::M: return &this->memory[registers.h << 8 | registers.l];
+        case SrcDestId8080::M: return &this->memory[(static_cast<uint16_t>(registers.h) << 8) | registers.l];
     }
+} 
+
+void CPU_8080::opLXI(RegisterPairId8080 rp, uint8_t dl, uint8_t dh) {
+    registers.setPair(rp, dl, dh);
+    registers.pc += 2;
 }
 
 void CPU_8080::opMOV(SrcDestId8080 dest, SrcDestId8080 src) {
-    *_getByteAddr(dest) = *_getByteAddr(src);
+    *_getAddr(dest) = *_getAddr(src);
 }
 
 void CPU_8080::opMVI(SrcDestId8080 dest, uint8_t data) {
-    *_getByteAddr(dest) = data;
+    *_getAddr(dest) = data;
     registers.pc++;
 }
 
@@ -30,17 +35,24 @@ bool CPU_8080::tick() {
     uint8_t nibble0 = (*opcode) & 0b11110000;
     uint8_t nibble1 = (*opcode) & 0b00001111;
 
+    if (nibble0 <= 3) {
+        if (nibble1 == 6 || nibble1 == 0xE) {
+            // MVI 0|0||D|D|D|1|1|0 + d8
+            opMVI(static_cast<SrcDestId8080>(*opcode & 0b00111000), opcode[1]);
+        }
+
+        if (nibble1 = 1) {
+            // LXI 0|0|R|P|0|0|0|1 + d16
+            opLXI(static_cast<RegisterPairId8080>(*opcode & 0b00110000), opcode[1], opcode[2]);
+        }
+    }
+
     if (nibble0 >= 4 && nibble0 <= 7) {
         if (nibble1 == 6) { /* HLT */ }
 
         // MOV 0|1|D|D|D|S|S|S
         opMOV(static_cast<SrcDestId8080>(*opcode & 0b00111000), static_cast<SrcDestId8080>(*opcode & 0b00000111));
         return true;
-    }
-
-    if ((nibble1 == 6 || nibble1 == 0xE) && (nibble0 <= 3)) {
-        // MVI 0|0||D|D|D|1|1|0
-        opMVI(static_cast<SrcDestId8080>(*opcode & 0b00111000), opcode[1]);
     }
 
     switch (*opcode) {
--- src/emu/emu.hpp
+++ src/emu/emu.hpp
@@ -20,6 +20,35 @@ struct Registers8080 {
     uint8_t l;
     uint16_t sp;
     uint16_t pc;
+
+    uint16_t getPair(RegisterPairId8080 rp) const {
+        switch (rp) {
+            case BC: return (static_cast<uint16_t>(b) << 8) | c;
+            case DE: return (static_cast<uint16_t>(d) << 8) | e;
+            case HL: return (static_cast<uint16_t>(h) << 8) | l;
+            case SP: return sp;
+        }
+    }
+
+    void setPair(RegisterPairId8080 rp, uint8_t dl, uint8_t dh) {
+        switch (rp) {
+            case BC:
+                b = dh;
+                c = dl;
+                break;
+            case DE:
+                d = dh;
+                e = dl;
+                break;
+            case HL: 
+                h = dh;
+                l = dl;
+                break;
+            case SP:
+                sp = (static_cast<uint16_t>(dh) << 8) | dl;
+                break;
+        }
+    }
 };
 
 enum SrcDestId8080 {
@@ -33,6 +62,13 @@ enum SrcDestId8080 {
     M
 };
 
+enum RegisterPairId8080 {
+    BC,
+    DE,
+    HL,
+    SP
+};
+
 class CPU_8080 {
 private:
     ConditionFlags8080 conditionFlags;
@@ -41,8 +77,9 @@ private:
     uint8_t* memory;
     bool intEnable;
 
-    uint8_t* _getByteAddr(SrcDestId8080 id);
+    uint8_t* _getAddr(SrcDestId8080 id);
 
+    void opLXI(RegisterPairId8080 rp, uint8_t dl, uint8_t dh);
     void opMOV(SrcDestId8080 dest, SrcDestId8080 src);
     void opMVI(SrcDestId8080 dest, uint8_t data);
 
