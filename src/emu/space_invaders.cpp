1b591286bfa4a1207d8a4126849d0aea480cc767
romcol <e.rcolombo2@gmail.com>
2026-01-13 20:56:48 -0300

    feat: controller input peripheral

--- src/emu/space_invaders.cpp
+++ src/emu/space_invaders.cpp
@@ -1,21 +1,61 @@
 #include "space_invaders.hpp"
 
+// Shift Register:
+uint8_t SpaceInvadersShiftRegister::getResult() {
+    uint16_t v = (shift1<<8) | shift0;    
+    return ((v >> (8 - shiftOffset)) & 0xff);
+}
+
+void SpaceInvadersShiftRegister::fill(uint8_t value) {
+    shift0 = shift1;    
+    shift1 = value;  
+}
+
+void SpaceInvadersShiftRegister::setOffset(uint8_t value) {
+    shiftOffset = value & 0b111;
+}
+
+
+// Controller Input:
+void SpaceInvadersControllerInput::setInputState(SpaceInvadersControllerInputId inputId, bool state) {
+    inputStates[static_cast<int>(inputId)] = state;
+}
+
+const std::array<bool, 9> SpaceInvadersControllerInput::getInputStates() {
+    return inputStates;
+}
+
+std::pair<uint8_t, uint8_t> SpaceInvadersControllerInput::getControllerInputPortsData() {
+    uint8_t port1, port2;
+    port1 = inputStates[COIN] | (inputStates[P2_START] << 1) | (inputStates[P1_START] << 2) | (inputStates[P1_SHOOT] << 4)
+        | (inputStates[P1_LEFT] << 5) | (inputStates[P1_RIGHT] << 6);
+    port2 = (inputStates[P2_SHOOT] << 4) | (inputStates[P2_LEFT] << 5) | (inputStates[P2_RIGHT] << 6);
+
+    return {port1, port2};
+}
+
+
+// Machine:
 uint8_t SpaceInvadersMachine::cpuIoInHandler(uint8_t port) {
-    switch(port) {    
+    switch(port) {   
+        case 1:
+            return controllerInput.getControllerInputPortsData().first;
+        case 2:
+             return controllerInput.getControllerInputPortsData().second;
         case 3: // Shift Register result
-            uint16_t v = (shift1<<8) | shift0;    
-            return ((v >> (8 - shiftOffset)) & 0xff);    
-    }    
+            return shiftRegister.getResult();  
+    }
+
+    return 0;
 }
 
 void SpaceInvadersMachine::cpuIoOutHandler(uint8_t port, uint8_t value) {
     switch (port) {
         case 2: // shift register result offset (bits 0,1,2)
-            shiftOffset = value & 0b111;
+            shiftRegister.setOffset(value);
             break;
         case 4: // fill shift register
-            shift0 = shift1;    
-            shift1 = value;    
+            shiftRegister.fill(value);
             break;
     }
 }
--- src/emu/space_invaders.hpp
+++ src/emu/space_invaders.hpp
@@ -1,17 +1,50 @@
 #pragma once
+#include <array>
 #include "cpu8080.hpp"
 
-class SpaceInvadersMachine {
+class SpaceInvadersShiftRegister {
 private:
-    CPU_8080 cpu;
-
-    // Shift register
     uint8_t shiftOffset;
     uint8_t shift0;
     uint8_t shift1;
 
+public:
+    uint8_t getResult();
+    void fill(uint8_t value);
+    void setOffset(uint8_t value);
+};
+
+enum SpaceInvadersControllerInputId {
+    COIN,
+    P1_START,
+    P1_SHOOT,
+    P1_LEFT,
+    P1_RIGHT,
+    P2_START,
+    P2_SHOOT,
+    P2_LEFT,
+    P2_RIGHT
+};
+
+class SpaceInvadersControllerInput {
+private:
+    std::array<bool, 9> inputStates;
+
+public:
+    void setInputState(SpaceInvadersControllerInputId inputId, bool state);
+
+    const std::array<bool, 9> getInputStates();
+    std::pair<uint8_t, uint8_t> getControllerInputPortsData();
+};
+
+class SpaceInvadersMachine {
+private:
+    CPU_8080 cpu;
     uint8_t cpuIoInHandler(uint8_t port);
     void cpuIoOutHandler(uint8_t port, uint8_t value);
 
+    // Peripherals:
+    SpaceInvadersShiftRegister shiftRegister;
+    SpaceInvadersControllerInput controllerInput;
 
 };
